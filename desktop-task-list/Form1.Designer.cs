fdb708f572db8d264fd5842e150e23ac6cdafe52
Christopher Aram Swayne <42748779+c-swa@users.noreply.github.com>
2020-11-29 19:39:07 -0600

    update stuff for init :)

--- desktop-task-list/Form1.Designer.cs
+++ desktop-task-list/Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace desktop_task_list
+﻿
+
+namespace desktop_task_list
 {
     partial class Form1
     {
@@ -22,16 +24,26 @@
 
         #region Windows Form Designer generated code
 
+        
+
         /// <summary>
         /// Required method for Designer support - do not modify
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(692, 515);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- desktop-task-list/Form1.cs
+++ desktop-task-list/Form1.cs
@@ -16,5 +16,27 @@ namespace desktop_task_list
         {
             InitializeComponent();
         }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            // Creating and setting the properties of the label
+            Label lb = new Label();
+            lb.Location = new Point(243, 80);
+            lb.Text = "Task List View";
+
+            // Adding Label control to the form
+            this.Controls.Add(lb);
+
+            // Creating and setting the properties of ListBox
+            ListView listView = new ListView();
+            listView.Location = new Point(246, 104);
+            listView.Visible = true;
+            listView.Items.Add("test 1");
+            listView.Items.Add("test 2");
+            listView.Items.Add("test 3");
+
+            this.Controls.Add(listView);
+
+        }
     }
 }
