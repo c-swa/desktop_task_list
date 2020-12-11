1d2da7580e86c1069b3f7631213e92b758b24b7b
Christopher Aram Swayne <42748779+c-swa@users.noreply.github.com>
2020-12-10 21:15:54 -0600

    new test html

--- obj/Debug/net5.0/RazorDeclaration/Pages/Index.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Pages/Index.razor.g.cs
@@ -13,77 +13,77 @@ namespace desktop_task_list.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "B:\desktop_task_list\desktop-task-list\_Imports.razor"
+#line 1 "C:\Users\Aram\Documents\UMKC\GitHub\desktop_task_list\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "B:\desktop_task_list\desktop-task-list\_Imports.razor"
+#line 2 "C:\Users\Aram\Documents\UMKC\GitHub\desktop_task_list\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "B:\desktop_task_list\desktop-task-list\_Imports.razor"
+#line 3 "C:\Users\Aram\Documents\UMKC\GitHub\desktop_task_list\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "B:\desktop_task_list\desktop-task-list\_Imports.razor"
+#line 4 "C:\Users\Aram\Documents\UMKC\GitHub\desktop_task_list\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "B:\desktop_task_list\desktop-task-list\_Imports.razor"
+#line 5 "C:\Users\Aram\Documents\UMKC\GitHub\desktop_task_list\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "B:\desktop_task_list\desktop-task-list\_Imports.razor"
+#line 6 "C:\Users\Aram\Documents\UMKC\GitHub\desktop_task_list\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "B:\desktop_task_list\desktop-task-list\_Imports.razor"
+#line 7 "C:\Users\Aram\Documents\UMKC\GitHub\desktop_task_list\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "B:\desktop_task_list\desktop-task-list\_Imports.razor"
+#line 8 "C:\Users\Aram\Documents\UMKC\GitHub\desktop_task_list\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "B:\desktop_task_list\desktop-task-list\_Imports.razor"
+#line 9 "C:\Users\Aram\Documents\UMKC\GitHub\desktop_task_list\_Imports.razor"
 using desktop_task_list;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "B:\desktop_task_list\desktop-task-list\_Imports.razor"
+#line 10 "C:\Users\Aram\Documents\UMKC\GitHub\desktop_task_list\_Imports.razor"
 using desktop_task_list.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "B:\desktop_task_list\desktop-task-list\Pages\Index.razor"
+#line 3 "C:\Users\Aram\Documents\UMKC\GitHub\desktop_task_list\Pages\Index.razor"
 using BlazorTodoApp.Models;
 
 #line default
@@ -98,7 +98,7 @@ using BlazorTodoApp.Models;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 29 "B:\desktop_task_list\desktop-task-list\Pages\Index.razor"
+#line 29 "C:\Users\Aram\Documents\UMKC\GitHub\desktop_task_list\Pages\Index.razor"
  
     public string todoInput {get; set; }
     public List<Todo> todos = new List<Todo>();
--- obj/Debug/net5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
@@ -13,70 +13,70 @@ namespace desktop_task_list.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "B:\desktop_task_list\desktop-task-list\_Imports.razor"
+#line 1 "C:\Users\Aram\Documents\UMKC\GitHub\desktop_task_list\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "B:\desktop_task_list\desktop-task-list\_Imports.razor"
+#line 2 "C:\Users\Aram\Documents\UMKC\GitHub\desktop_task_list\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "B:\desktop_task_list\desktop-task-list\_Imports.razor"
+#line 3 "C:\Users\Aram\Documents\UMKC\GitHub\desktop_task_list\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "B:\desktop_task_list\desktop-task-list\_Imports.razor"
+#line 4 "C:\Users\Aram\Documents\UMKC\GitHub\desktop_task_list\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "B:\desktop_task_list\desktop-task-list\_Imports.razor"
+#line 5 "C:\Users\Aram\Documents\UMKC\GitHub\desktop_task_list\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "B:\desktop_task_list\desktop-task-list\_Imports.razor"
+#line 6 "C:\Users\Aram\Documents\UMKC\GitHub\desktop_task_list\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "B:\desktop_task_list\desktop-task-list\_Imports.razor"
+#line 7 "C:\Users\Aram\Documents\UMKC\GitHub\desktop_task_list\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "B:\desktop_task_list\desktop-task-list\_Imports.razor"
+#line 8 "C:\Users\Aram\Documents\UMKC\GitHub\desktop_task_list\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "B:\desktop_task_list\desktop-task-list\_Imports.razor"
+#line 9 "C:\Users\Aram\Documents\UMKC\GitHub\desktop_task_list\_Imports.razor"
 using desktop_task_list;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "B:\desktop_task_list\desktop-task-list\_Imports.razor"
+#line 10 "C:\Users\Aram\Documents\UMKC\GitHub\desktop_task_list\_Imports.razor"
 using desktop_task_list.Shared;
 
 #line default
@@ -90,7 +90,7 @@ using desktop_task_list.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 18 "B:\desktop_task_list\desktop-task-list\Shared\NavMenu.razor"
+#line 18 "C:\Users\Aram\Documents\UMKC\GitHub\desktop_task_list\Shared\NavMenu.razor"
        
     private bool collapseNavMenu = true;
 
